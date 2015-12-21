eaa535db394c710b84a8c2ab4d40bccb7a78e436
Alexander Ryasnyansky <aryasnyansky@smartlabs.tv>
2015-12-21 18:40:47 +0300

    fix

--- ya-translate.py
+++ ya-translate.py
@@ -23,29 +23,29 @@ if code == 200:
     translate = data['text'][0]
     print(text + ' | ' + translate)
 
-    if (add == '-a'):
-        # csv
-        founded = False
-        file_path = os.path.dirname(os.path.realpath(__file__))
-        with open(os.path.join(file_path, file_name), 'r', newline='') as csvfile:
-            csv_reader = csv.reader(csvfile, delimiter=',')
-            for row in csv_reader:
-                if len(row) > 0 and ((text == row[2] and translate == row[3]) or (text == row[3] and translate == row[2])):
-                    print('This word/phrase was translated already')
-                    founded = True
-                    break
-        if (not founded) and (text != translate):
-            with open(os.path.join(file_path, file_name), 'a', newline='') as csvfile:
-                csv_writer = csv.writer(csvfile, delimiter=',',
-                                        quotechar='|', quoting=csv.QUOTE_MINIMAL)
-                if lang == 'en-ru':
-                    csv_writer.writerow(
-                            ['English'] + ['Russian'] + [text] + [translate])
-                if lang == 'ru-en':
-                    csv_writer.writerow(
-                            ['Russian'] + ['English'] + [text] + [translate])
-                print('Word/phrase was added to a dictionary')
-    else:
+
+    # csv
+    founded = False
+    file_path = os.path.dirname(os.path.realpath(__file__))
+    with open(os.path.join(file_path, file_name), 'r', newline='') as csvfile:
+        csv_reader = csv.reader(csvfile, delimiter=',')
+        for row in csv_reader:
+            if len(row) > 0 and ((text == row[2] and translate == row[3]) or (text == row[3] and translate == row[2])):
+                print('This word/phrase was translated already')
+                founded = True
+                break
+    if (add == '-a') and (not founded) and (text != translate):
+        with open(os.path.join(file_path, file_name), 'a', newline='') as csvfile:
+            csv_writer = csv.writer(csvfile, delimiter=',',
+                                    quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            if lang == 'en-ru':
+                csv_writer.writerow(
+                        ['English'] + ['Russian'] + [text] + [translate])
+            if lang == 'ru-en':
+                csv_writer.writerow(
+                        ['Russian'] + ['English'] + [text] + [translate])
+            print('Word/phrase was added to a dictionary')
+    elif (not founded):
         print('If you want to add this word to local dictionary, add -a to request.')
 else:
     print('Wrong request!')
